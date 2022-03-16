ec5ca2956f96d64d20491fe221f74ec762c2f637
Jianping Lu <jianpl@microsoft.com>
2022-03-16 15:52:10 -0700

    first algorim checkin

---
+++ Algorithm/AlgorithmProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
